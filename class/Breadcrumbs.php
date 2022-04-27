aae3e7f340a7e3aeb1bc43b601b9f909f2c6c3be
fwmakc <fwmakc@mail.ru>
2022-04-27 11:29:11 +0300

    1.0

--- class/Breadcrumbs.php
+++ class/Breadcrumbs.php
@@ -7,21 +7,36 @@ use is\Helpers\Objects;
 use is\Helpers\Strings;
 
 use is\Components\Uri;
+use is\Components\Router;
 
 use is\Masters\Modules\Master;
 use is\Masters\View;
 
-class Breadcrumbs extends Master {
-	
-	public $route;
-	
-	public function launch() {
-		
-		$uri = Uri::getInstance();
-		$this -> route = $uri -> get('route');
-		
-	}
-	
-}
+class Breadcrumbs extends Master
+{
+    public $route;
 
-?>
+    public function launch()
+    {
+        $uri = Uri::getInstance();
+        $this->route = $uri->get('route');
+
+        $router = Router::getInstance();
+
+        //$this->content = $router->content;
+
+        $content = $router->content;
+        //if ($content['parents']) {
+        //    $content['parents'] = Objects::unfirst($content['parents']);
+        //    if ($content['parents']) {
+        //        $this->route = Objects::add($this->route, $content['parents']);
+        //    }
+        //}
+        if ($content['name']) {
+            $this->route = Objects::add($this->route, [ $content['name'] ]);
+        }
+
+        //System::debug($content);
+        //System::debug($this->route);
+    }
+}
